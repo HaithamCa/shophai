3770de05233e58be6365cd933a62297021f17470
Haaitham <hythamdefrawy@gmail.com>
2021-07-22 15:05:25 -0400

    Navigate to menu-item using withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
+import { Link, Route } from 'react-router-dom';
 
-const HomePage = () => (
-    <div className='homepage'>
-        <Directory />
-    </div>
-)
+
+const HomePage = () => {
+    return (
+        < div className='homepage' >
+            <Directory />
+        </div >
+    )
+}
+
+
+
 
 export default HomePage;

14c781fee9c30110d81eb871d90c09693958ed80
Haaitham <hythamdefrawy@gmail.com>
2021-07-27 10:29:29 -0400

    add collection preview component and collection item component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import SHOP_DATA from './shop.data';
+
+export class ShopPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+
+    render() {
+        const { collections } = this.state;
+        return (
+            <div>
+                {
+                    collections.map(({ id, ...collectionPreviewProps }) => (
+                        <CollectionPreview key={id} {...collectionPreviewProps} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default ShopPage
